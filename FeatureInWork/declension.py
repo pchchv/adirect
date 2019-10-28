c52d76ef5faa18ad4cd2dbd1470806e967f0ad5f
devqwil <51747787+devqwil@users.noreply.github.com>
2019-10-28 19:49:50 +0300

    up

--- FeatureInWork/declension.py
+++ FeatureInWork/declension.py
@@ -6,14 +6,22 @@ morph = pymorphy2.MorphAnalyzer()
 """Функция возвращает список склонений заданного слова, включая само слово"""
 
 
-def declension(word):
-    words = morph.parse(word.lower())[0].lexeme          #Создаётся список всех форм слова
-    decls = []
-    for element in words:                                 #В цикле поэлементно чистим список от лишних данных
-        decl = str(element).split(' ')
-        decls.append(decl[-3].strip(',').strip("'"))
-    if word not in decls:
-        decls.append(word)
-    return decls
+#UserInput = modifier(open('test.txt', 'r').read())
+#UserInput = 'айфон'
 
-#print(declension(input()))
+def declension(UserInput):
+    result = []
+    if type(UserInput) == str:
+        UserInput = UserInput.split(' ')
+    for word in UserInput:
+        words = morph.parse(word.lower())[0].lexeme          #Создаётся список всех форм слова
+        decls = []
+        for element in words:
+            decl = str(element).split(' ')
+            decls.append(decl[-3])
+        if word not in decls:
+            decls.append(word)
+        result.append(decls)
+    return result
+
+#print(declension(UserInput))
