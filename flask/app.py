dd32553c169b66d5c2cd3ccf288a64fadb3cece8
devqwil <atlasovnv@yandex.ru>
2019-11-03 19:49:01 +0300

    flask допилил меню и шаблоны

--- flask/app.py
+++ flask/app.py
@@ -3,34 +3,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    context = {
-        'title': 'Interior'
-    }
-    return render_template('index.html', context=context)
+
+    return render_template('index.html', title='Home', ServiceName='Сделаем это!')
 
 @app.route('/generator')
 def generator():
-    context = {
-        'title': 'Interior'
-    }
-    return render_template('generator.html', context=context)
+
+    return render_template('generator.html', title='Генератор фраз', ServiceName='Генератор фраз')
 
 @app.route('/crossminus')
 def crossminus():
-    return render_template('crossminus.html')
+    return render_template('crossminus.html', title='Кросс-минусовка фраз', ServiceName='Кросс-минусовка фраз')
 
 @app.route('/inclinator')
 def inclinator():
-    return render_template('inclinator.html')
+    return render_template('inclinator.html', title='Склонятор', ServiceName='Склонение ключевых слов')
 
 @app.route('/lemmatizer')
 def lemmatizer():
-    return render_template('lemmatizer.html')
+    return render_template('lemmatizer.html', title='Лемматизатор', ServiceName='Лемматизатор')
 
 @app.route('/synonymizer')
 def synonymizer():
-    return render_template('synonymizer.html')
+    return render_template('synonymizer.html', title='Синонимайзер', ServiceName='Синонимайзер')
 
 @app.route('/wordcount')
 def wordcount():
-    return render_template('wordcount.html')
+    return render_template('wordcount.html', title='Считалка слов', ServiceName='Считалка слов')
